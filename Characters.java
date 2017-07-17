d2ec610b7a55c97dec1ccf370afa2a14bdcd6dfd
kavinkumar06 <kavinm09@gmail.com>
2017-07-17 12:04:13 +0530

    Create Characters.java

---
+++ Characters.java
@@ -0,0 +1,19 @@
+# Characters
+import java.io.*;
+import java.util.*;
+public class Characters
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+		
+		String j=sc.next();
+		int count=0;
+		for(int i=0;i<j.length();i++)
+    {
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
